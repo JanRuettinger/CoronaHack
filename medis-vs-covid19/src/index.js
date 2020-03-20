859447da5b7f32d6e70ee5f5289d3529d1a37b6b
Svilen Stefanov <svilen.ks@gmail.com>
2020-03-21 00:39:22 +0200

    attempt to fix build

--- medis-vs-covid19/src/index.js
+++ medis-vs-covid19/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var config = {}
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
