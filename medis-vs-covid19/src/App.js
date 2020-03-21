c38788a46cd6a4e450e3a3bb94893480bf8c651a
Svilen Stefanov <svilen.ks@gmail.com>
2020-03-21 03:33:06 +0200

    Navigation bar

--- medis-vs-covid19/src/App.js
+++ medis-vs-covid19/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import Navbar from './Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +21,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> 
+      </header>*/}
     </div>
   );
 }
--- medis-vs-covid19/src/index.js
+++ medis-vs-covid19/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 // import * as firebase from 'firebase';
 
@@ -9,7 +10,11 @@ import * as serviceWorker from './serviceWorker';
 
 // firebase.initializeApp(config);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
